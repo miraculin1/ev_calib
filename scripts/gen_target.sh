3389c481878dc6d65f5ba3b573d53e770ac10f5f
miraculin <2855193562@qq.com>
2026-04-23 15:48:11 +0800

    fix dv-ros version problem

--- scripts/gen_target.sh
+++ scripts/gen_target.sh
@@ -1,1 +1,1 @@
-rosrun kalibr kalibr_create_target_pdf --type apriltag --nx 4 --ny 4 --tsize 0.05 --tspace 0.5
+rosrun kalibr kalibr_create_target_pdf --type apriltag --nx 4 --ny 4 --tsize 0.005 --tspace 0.5
